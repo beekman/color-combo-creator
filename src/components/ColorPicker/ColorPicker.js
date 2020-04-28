d0e072f730dad38d3b62a08ac170a91c819ca880
Ben Beekman <bbeekman@gmail.com>
2020-04-28 16:12:07 -0700

    colorpicker test and snapshot update

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,11 +1,14 @@
-let scm = new ColorScheme;
-scm.from_hue(21)
+var ColorScheme = require('color-scheme');
+
+var scheme = new ColorScheme;
+
+scheme.from_hue(21)
   .scheme('triade')
   .distance(0.1)
   .add_complement(false)
   .variation('pastel')
   .web_safe(true);
 
-var colors = scm.colors();
+var colors = scheme.colors();
 
 console.log(colors);
