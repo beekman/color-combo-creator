179efc41f27f1ed2724670db4eb385c09feaf3f2
Ben Beekman <bbeekman@gmail.com>
2020-05-03 15:42:08 -0700

    color pickr component rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import Header from './Header/Header';
 import VariationsControls from './VariationsControls/VariationsControls';
-import Picker from './Picker/Picker';
+import ColorPicker from '@mapbox/react-colorpickr';
 import LivePalette from './LivePalette/LivePalette';
 import ScratchPalette from './ScratchPalette/ScratchPalette';
 import Footer from './Footer/Footer';
-export default function App() {
-  return (
-    <>
-      <Header />
-      <Picker />
-      <VariationsControls />
-      <LivePalette />
-      <ScratchPalette />
-      <Footer />
-    </>
-  );
+import './colorpickr.css';
+
+
+class App extends React.PureComponent {
+  onChange = color => {
+    console.log(color);
+  };
+  render() {
+    return (
+      <>
+        <Header />
+        <ColorPicker onChange={this.onChange} />
+        <VariationsControls />
+        <LivePalette />
+        <ScratchPalette />
+        <Footer />
+      </>
+    )
+  }
 }
+
+export default App;
