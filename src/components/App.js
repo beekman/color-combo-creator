a9f17277d9ad50185858e1e5e21274a45a872059
Ben Beekman <bbeekman@gmail.com>
2020-05-02 17:15:46 -0700

    extracting out of unnecessary container component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import PalettePicker from '../containers/PalettePicker';
-
+import VariationsControls from './VariationsControls/VariationsControls';
+import Picker from './Picker/Picker';
+import LivePalette from './LivePalette/LivePalette';
+import ScratchPalette from './ScratchPalette/ScratchPalette';
 export default function App() {
-  return <PalettePicker />;
+  return (
+    <>
+      <Picker />
+      <VariationsControls />
+      <LivePalette />
+      <ScratchPalette />
+    </>
+  );
 }
