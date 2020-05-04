29620dcc7219556060b00e143efda21d7f154813
Ben Beekman <bbeekman@gmail.com>
2020-05-03 18:21:00 -0700

    switching color pickers again, found a better option

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,24 @@ import React from 'react';
 import Header from './Header/Header';
 import VariationsControls from './VariationsControls/VariationsControls';
 import ColorPicker from '@mapbox/react-colorpickr';
+import { SketchPicker } from 'react-color';
 import LivePalette from './LivePalette/LivePalette';
 import ScratchPalette from './ScratchPalette/ScratchPalette';
 import Footer from './Footer/Footer';
-import './colorpickr.css';
 
 
 class App extends React.PureComponent {
-  onChange = color => {
-    console.log(color);
-  };
   render() {
     return (
       <>
         <Header />
-        <ColorPicker onChange={this.onChange} />
+        <SketchPicker />
         <VariationsControls />
         <LivePalette />
         <ScratchPalette />
         <Footer />
       </>
-    )
+    );
   }
 }
 
