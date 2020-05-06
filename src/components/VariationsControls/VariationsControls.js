bae86a3d58e1eb902602ea753f183e510043aeb5
Ben Beekman <bbeekman@gmail.com>
2020-05-05 23:19:18 -0700

    Connected all Variations input values to state

--- src/components/VariationsControls/VariationsControls.js
+++ src/components/VariationsControls/VariationsControls.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from '../VariationsControls/VariationsControls.css';
 
+
+
 const VariationsControls = () => {
+  const [harmonies, setHarmonies] = useState('2');
+  const [inverses, setInverses] = useState('0');
+  const [lighters, setLighters] = useState('0');
+  const [darkers, setDarkers] = useState('0');
+
   return (
+
     <div className={styles.VariationsControls}>
-      <label htmlFor="harmonies">Harmonies</label><input type="number" id="harmonies" value="0" />
-      <label htmlFor="inverse">Inverses</label><input type="number" id="inverses" value="0" />
-      <label htmlFor="lighter">Lighter</label><input type="number" id="lighter" value="0" />
-      <label htmlFor="darker">Darker</label><input type="number" id="darker" value="0" />
+      <label htmlFor="harmonies">Harmonies</label><input type="number" id="harmonies" value={harmonies} onChange={({ target }) => setHarmonies(target.value)} />
+      <label htmlFor="inverse">Inverses</label><input type="number" id="inverses" value={inverses} onChange={({ target }) => setInverses(target.value)} />
+      <label htmlFor="lighter">Lighter</label><input type="number" id="lighter" value={lighters} onChange={({ target }) => setLighters(target.value)} />
+      <label htmlFor="darker">Darker</label><input type="number" id="darker" value={darkers} onChange={({ target }) => setDarkers(target.value)} />
     </div >
   );
 };
