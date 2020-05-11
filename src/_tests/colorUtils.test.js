c2b3bdc55f4524cf20e89be5dd3e18b5f1dd2861
Ben Beekman <bbeekman@gmail.com>
2020-05-11 15:08:39 -0700

    refactoring: more descriptive variable names for harmonies and better descriptions for tests

--- src/_tests/colorUtils.test.js
+++ src/_tests/colorUtils.test.js
@@ -1,6 +1,6 @@
 const { getHarmonies } = require('../utils/colorUtils');
 
-describe('tests for getHarmonies functions', () => {
+describe('tests for getHarmonies function', () => {
   it('should return an empty array for complements of 0', () => {
     const hsl = { h: 180, s: 1, l: 0.5, a: 1 };
     const harmonies = 0;
@@ -13,7 +13,7 @@ describe('tests for getHarmonies functions', () => {
     expect(getHarmonies(hsl, harmonies)).toEqual([{ h: 0, s: 1, l: 0.5, a: 1 }]);
   });
 
-  it('should return 2 colors with equidistant hues above and below color with h:180 and 2 harmonies', () => {
+  it('should return 2 colors with equidistant hues above and below for 2 harmonies and color with h:180', () => {
     const hsl = { h: 180, s: 1, l: 0.5, a: 1 };
     const harmonies = 2;
     expect(getHarmonies(hsl, harmonies)).toEqual([{ h: 60, s: 1, l: 0.5, a: 1 }, { h: 300, s: 1, l: 0.5, a: 1 }]);
@@ -25,18 +25,18 @@ describe('tests for getHarmonies functions', () => {
   });
 
 
-  it('should return color of hue 180 for 1 harmony and a base color with hue of 0', () => {
+  it('should return color of hue 180 for 1 harmony and a base color with h:0', () => {
     const hsl = { h: 0, s: 1, l: 0.5, a: 1 };
     const harmonies = 1;
     expect(getHarmonies(hsl, harmonies)).toEqual([{ 'a': 1, 'h': 180, 'l': 0.5, 's': 1 }]);
   });
-  it('should return 2 colors with hues above 0 for 2 harmonies and base hue of 0', () => {
+  it('should return 2 colors with hues above 0 for 2 harmonies and base color with h:0', () => {
     const hsl = { h: 0, s: 1, l: 0.5, a: 1 };
     const harmonies = 2;
     expect(getHarmonies(hsl, harmonies)).toEqual([{ h: 120, s: 1, l: 0.5, a: 1 },
     { h: 240, s: 1, l: 0.5, a: 1 }]);
   });
-  it('should return 3 colors with hues above 0 for 3 harmonies and base hue of 0', () => {
+  it('should return 3 colors with hues above 0 for 3 harmonies and base color with h:0', () => {
     const hsl = { h: 0, s: 1, l: 0.5, a: 1 };
     const harmonies = 3;
     expect(getHarmonies(hsl, harmonies)).toEqual([{ h: 90, s: 1, l: 0.5, a: 1 },
@@ -44,18 +44,18 @@ describe('tests for getHarmonies functions', () => {
     { h: 270, s: 1, l: 0.5, a: 1 }]);
   });
 
-  it('should return color of hue 179 for 1 harmony and color with base hue of 359', () => {
+  it('should return color with hue 179 for 1 harmony and base color with h:359', () => {
     const hsl = { h: 359, s: 1, l: 0.5, a: 1 };
     const harmonies = 1;
     expect(getHarmonies(hsl, harmonies)).toEqual([{ 'a': 1, 'h': 179, 'l': 0.5, 's': 1 }]);
   });
-  it('should return colors of hues 119 and 239 for 2 harmonies and color with a base hue of 359', () => {
+  it('should return colors of hues 119 and 239 for 2 harmonies and base color with h:359', () => {
     const hsl = { h: 359, s: 1, l: 0.5, a: 1 };
     const harmonies = 2;
     expect(getHarmonies(hsl, harmonies)).toEqual([{ h: 239, s: 1, l: 0.5, a: 1 },
     { h: 119, s: 1, l: 0.5, a: 1 }]);
   });
-  it('should return three colors with hues below 359 for 3 harmonies and color with a base hue of 359', () => {
+  it('should return three colors with hues below 359 for 3 harmonies and base color with h:359', () => {
     const hsl = { h: 359, s: 1, l: 0.5, a: 1 };
     const harmonies = 3;
     expect(getHarmonies(hsl, harmonies)).toEqual([{ h: 269, s: 1, l: 0.5, a: 1 },
@@ -63,3 +63,6 @@ describe('tests for getHarmonies functions', () => {
     { h: 89, s: 1, l: 0.5, a: 1 }]);
   });
 });
+
+describe('tests for getHarmonies function', () => {
+  it('should return an empty array for opposite of 0', () => {
--- src/components/VariationsControls/VariationsControls.js
+++ src/components/VariationsControls/VariationsControls.js
@@ -5,14 +5,14 @@ import styles from '../VariationsControls/VariationsControls.css';
 import { getHarmonies } from '../../utils/colorUtils';
 
 const VariationsControls = (color) => {
-  const [harmonies, setHarmonies] = useState('2');
+  const [harmonyQuantity, setHarmonyQuantity] = useState('2');
   const [inverses, setInverses] = useState('0');
   const [lighters, setLighters] = useState('0');
   const [darkers, setDarkers] = useState('0');
 
   useEffect(() => {
     const hsl = color.hsl;
-    getHarmonies(color.hsl, harmonies);
+    getHarmonies(color.hsl, harmonyQuantity);
   });
 
   return (
@@ -20,7 +20,7 @@ const VariationsControls = (color) => {
       <section className={styles.ColorMatches}>
       </section>
       <div className={styles.VariationsControls}>
-        <label htmlFor="harmonies">Harmonies</label><input type="number" id="harmonies" value={harmonies} onChange={({ target }) => setHarmonies(target.value)} />
+        <label htmlFor="harmonyQuantity">Harmonies</label><input type="number" id="harmonyQuantity" value={harmonyQuantity} onChange={({ target }) => setHarmonyQuantity(target.value)} />
         <label htmlFor="inverse">Inverses</label><input type="number" id="inverses" value={inverses} onChange={({ target }) => setInverses(target.value)} />
         <label htmlFor="lighter">Lighter</label><input type="number" id="lighter" value={lighters} onChange={({ target }) => setLighters(target.value)} />
         <label htmlFor="darker">Darker</label><input type="number" id="darker" value={darkers} onChange={({ target }) => setDarkers(target.value)} />
