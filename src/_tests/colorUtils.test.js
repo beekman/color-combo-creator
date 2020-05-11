d88793c721dd4bc8444359ab535c6b334764435a
Ben Beekman <bbeekman@gmail.com>
2020-05-10 23:14:37 -0700

    two tests for max hue edge case

--- src/_tests/colorUtils.test.js
+++ src/_tests/colorUtils.test.js
@@ -7,3 +7,17 @@ describe('should return colors with evenly spaced hues above a color with a hue 
     expect(getHarmonies(hsl, harmonies)).toEqual([{ 'a': 1, 'h': 180, 'l': 0.5, 's': 1 }]);
   });
 });
+
+describe('should return colors with evenly spaced hues below a color with a hue of 359', () => {
+  it('should return 179 with base hue of 359 and 1 harmony', () => {
+    const hsl = { h: 359, s: 1, l: 0.5, a: 1 };
+    const harmonies = 1;
+    expect(getHarmonies(hsl, harmonies)).toEqual([{ 'a': 1, 'h': 179, 'l': 0.5, 's': 1 }]);
+  });
+  it('should return 119 and 239 with base hue of 359 and 1 harmony', () => {
+    const hsl = { h: 359, s: 1, l: 0.5, a: 1 };
+    const harmonies = 2;
+    expect(getHarmonies(hsl, harmonies)).toEqual([{ h: 239, s: 1, l: 0.5, a: 1 },
+    { h: 119, s: 1, l: 0.5, a: 1 }]);
+  });
+});
