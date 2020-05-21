e171d4a57190d01a5c3dc08afbedaebc1aebf61d
Ben Beekman <bbeekman@gmail.com>
2020-05-21 15:21:29 -0700

    refactoring and improved palette style names for exports

--- src/components/VariationsControls/VariationsControls.js
+++ src/components/VariationsControls/VariationsControls.js
@@ -31,33 +31,27 @@ const VariationsControls = (color) => {
   useEffect(() => {
     setInverseMax(harmonyQuantity + 1);
     const hslHarmonies = getHarmonies(color.color, harmonyQuantity);
-    makeColorSwatches(hslHarmonies);
-
-
     const hslInverses = getInverses(color.color, hslHarmonies, inverseQuantity);
-    makeColorSwatches(hslInverses);
-
     baseHarmoniesAndInversesColorList = getBaseHarmoniesAndInversesColorList(color.color, hslHarmonies, hslInverses);
     hslLighters = getLighters(baseHarmoniesAndInversesColorList, lighterQuantity);
-    makeColorSwatches(hslLighters);
     hslDarkers = getDarkers(baseHarmoniesAndInversesColorList, darkerQuantity);
-    makeColorSwatches(hslDarkers);
     hslDesaturateds = getDesaturateds(baseHarmoniesAndInversesColorList, desaturatedQuantity);
-    makeColorSwatches(hslDesaturateds);
-    postcssHarmonies = getPostcssValuesVariables(baseHarmoniesAndInversesColorList);
-    console.log(postcssHarmonies);
-    setStyleExportText(postcssHarmonies);
+    const postcssValuesVariables = (getPostcssValuesVariables(hslHarmonies)) + (getPostcssValuesVariables(hslInverses)) + (getPostcssValuesVariables(hslLighters)) + (getPostcssValuesVariables(hslDarkers)) + (getPostcssValuesVariables(hslDesaturateds));
 
+    makeColorSwatches(hslHarmonies);
+    makeColorSwatches(hslInverses);
+    makeColorSwatches(hslLighters);
+    makeColorSwatches(hslDarkers);
+    makeColorSwatches(hslDesaturateds);
+    setStyleExportText(postcssValuesVariables);
   });
 
   let hslHarmonies = getHarmonies(color.color, harmonyQuantity);
-
   let hslInverses = getInverses(color.color, hslHarmonies, inverseQuantity);
   let baseHarmoniesAndInversesColorList = getBaseHarmoniesAndInversesColorList(color.color, hslHarmonies, hslInverses);
   let hslLighters = getLighters(baseHarmoniesAndInversesColorList, lighterQuantity);
   let hslDarkers = getDarkers(baseHarmoniesAndInversesColorList, darkerQuantity);
   let hslDesaturateds = getDesaturateds(baseHarmoniesAndInversesColorList, desaturatedQuantity);
-  let postcssHarmonies = getPostcssValuesVariables(baseHarmoniesAndInversesColorList);
 
 
 
@@ -87,11 +81,10 @@ const VariationsControls = (color) => {
   return (
     <>
       <div className={styles.VariationsControls} style={{ background: `hsl(${(color.color.h)}, ${color.color.s * 100}%, ${color.color.l * 150}%)`, borderWidth: '2px', borderColor: `hsl(${(getOppositeDegree(color.color.h))}, ${color.color.s * 100}%, ${color.color.l * 100}%)`, borderStyle: 'solid' }}>
-
-        <label htmlFor="harmonyQuantity" title="Complementary colors to generate, evenly spaced around the color wheel. 2 will give you a split complementary triad scheme, 3 will return a color from each quarter of the color wheel."><IoIosColorFilter /><span className={styles.textLabel}>Harmonies</span></label><input type="number" id="harmonyQuantity" value={harmonyQuantity} min="0" max="36" onChange={({ target }) => setHarmonyQuantity(target.value)} />
-        <label htmlFor="inverseQuantity" title="Colors opposite from the base & harmonic colors on the color wheel. First color is inverted base, subsequent colors are inverted harmonies."><MdInvertColors /><span className={styles.textLabel}>Inverses</span></label><input type="number" id="inverseQuantity" min="0" max={inverseMax} value={inverseQuantity} onChange={({ target }) => setInverseQuantity(target.value)} />
+        <label htmlFor="harmonyQuantity" title="Complementary colors, evenly spaced around the color wheel, relative to the base hue. 2 will give you a split complementary triad scheme, 3 will return a color from each quarter of the color wheel."><IoIosColorFilter /><span className={styles.textLabel}>Harmonies</span></label><input type="number" id="harmonyQuantity" value={harmonyQuantity} min="0" max="36" onChange={({ target }) => setHarmonyQuantity(target.value)} />
+        <label htmlFor="inverseQuantity" title="Colors opposite from the base & harmonic colors on the color wheel. First color is inverted base, subsequent colors are inverted harmonies."><MdInvertColors /><span className={styles.textLabel}>Inverses</span></label><input type="number" id="inverseQuantity" min="0" max={harmonyQuantity} value={inverseQuantity} onChange={({ target }) => setInverseQuantity(target.value)} />
         <label htmlFor="lighterQuantity" title="Lighter color sets to generate from the base, harmonies, and inverses, with each increment stepping closer to white."><MdBrightnessLow /><span className={styles.textLabel}>Lighter</span> &times;<input type="number" id="lighterQuantity" value={lighterQuantity} min="0" max="20" onChange={({ target }) => setLighterQuantity(target.value)} /></label>
-        <label htmlFor="darkerQuantity" title="Darker color sets to generate from the base, harmonies, and inverses,with each increment stepping closer to black."> <TiAdjustBrightness /><span className={styles.textLabel}>Darker</span>&times;<input type="number" id="darkerQuantity" min="0" max="20" value={darkerQuantity} onChange={({ target }) => setDarkerQuantity(target.value)} /></label>
+        <label htmlFor="darkerQuantity" title="Darker color sets to generate from the base, harmonies, and inverses,with each increment stepping closer to black."> <TiAdjustBrightness /><span className={styles.textLabel}>Darker</span> &times;<input type="number" id="darkerQuantity" min="0" max="20" value={darkerQuantity} onChange={({ target }) => setDarkerQuantity(target.value)} /></label>
         <label htmlFor="desaturatedQuantity" title="Less saturated color sets to generate from the base, harmonies, and inverses, with each increment stepping closer to grayscale."><MdFormatColorReset /><span className={styles.textLabel}>Desaturated</span> &times;<input type="number" id="desaturatedQuantity" value={desaturatedQuantity} min="0" max="20" onChange={({ target }) => setDesaturatedQuantity(target.value)} /></label>
       </div>
 
@@ -102,9 +95,9 @@ const VariationsControls = (color) => {
         {darkerSwatches}
         {desaturatedSwatches}
       </section>
-      {postcssHarmonies}
       <section className={styles.export}>
-        <textarea className={styles.cssOutputText} value={styleExportText} onChange={({ postcssHarmonies }) => setStyleExportText(postcssHarmonies)} />
+        <h3>PostCSS Values Variables color list</h3>
+        <textarea className={styles.cssOutputText} value={styleExportText} onChange={({ postcssValuesVariables }) => setStyleExportText(postcssValuesVariables)} />
       </section>
     </>
   );
