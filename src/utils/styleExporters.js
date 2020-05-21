14b690ab70163a3ac633070db11b628089e8ca1b
Ben Beekman <bbeekman@gmail.com>
2020-05-21 16:51:35 -0700

    CSS export stylesheet now renders dynamic styles based on palette

--- src/utils/styleExporters.js
+++ src/utils/styleExporters.js
@@ -2,7 +2,7 @@ export const getPostcssValuesVariables = (colorSet) => {
   let postCSSVariables = '';
   if(colorSet.length > 0) {
     colorSet.map((color, i) => {
-      let key = (color.matchType + (Number(i) + 1));
+      let key = (color.matchType);
       let line = `${key}: hsl(${(color.h).toFixed(0)}, ${(color.s * 100).toFixed(2)}%, ${(color.l * 100).toFixed(2)}%);\n`;
       postCSSVariables += line;
     });
@@ -14,8 +14,8 @@ export const getCssClasses = (colorSet) => {
   let styles = '';
   if(colorSet.length > 0) {
     colorSet.map((color, i) => {
-      let key = (color.matchType + (Number(i) + 1));
-      let line = `${key}{ color: hsl(${(color.h).toFixed(0)}, ${(color.s * 100).toFixed(2)}%, ${(color.l * 100).toFixed(2)}%);}\n`;
+      let key = color.matchType;
+      let line = `${key} {\ncolor: hsl(${(color.h).toFixed(0)}, ${(color.s * 100).toFixed(2)}%, ${(color.l * 100).toFixed(2)}%);\n}\n`;
       styles += line;
     });
   }
