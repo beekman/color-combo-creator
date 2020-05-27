6486787226415ba2f29c9038181770d32e40fca8
Ben Beekman <bbeekman@gmail.com>
2020-05-26 22:12:30 -0700

    improved ux for hiding and showing of export options

--- src/components/VariationsControls/VariationsControls.js
+++ src/components/VariationsControls/VariationsControls.js
@@ -28,6 +28,10 @@ const VariationsControls = (color) => {
   const handleSwatchClick = () => setSwatchToggled((toggled) => !toggled);
   const [postcssExportText, setPostcssExportText] = useState('base: hsl(0, 100%, 50%);');
   const [cssExportText, setCssExportText] = useState('.base{\nhsl(0, 100%, 50%);\n}');
+  const [cssExportToggled, setCssExportToggled] = useState(true);
+  const handleCssExportClick = () => setCssExportToggled((toggled) => !toggled);
+  const [postcssExportToggled, setPostcssExportToggled] = useState(true);
+  const handlePostcssExportClick = () => setPostcssExportToggled((toggled) => !toggled);
 
   useEffect(() => {
     setInverseMax(Number(harmonyQuantity) + 1);
@@ -63,7 +67,7 @@ const VariationsControls = (color) => {
         return (
           <div key={key} style={{ background: `hsl(${color.h}, ${color.s * 100}%, ${color.l * 100}%)` }} className={styles.Swatch} onClick={handleSwatchClick}>
             <aside className={`${styles.details} ${swatchToggled && styles.hidden}`}>
-             <strong>{(color.matchType)}</strong> hsl({(color.h).toFixed(0)}, {(color.s * 100).toFixed(2)}%, {(color.l * 100).toFixed(2)}%)
+              <strong>{(color.matchType)}</strong> hsl({(color.h).toFixed(0)}, {(color.s * 100).toFixed(2)}%, {(color.l * 100).toFixed(2)}%)
             </aside>
           </div>
         );
@@ -97,10 +101,10 @@ const VariationsControls = (color) => {
       </section>
       <section className={styles.export}>
         <h2>Palette Export Options</h2>
-        <h3><a href="https://github.com/css-modules/css-modules/blob/master/docs/values-variables.md" target="_blank">PostCSS Values Variables</a> color list</h3>
-        <textarea className={styles.cssOutputText} value={postcssExportText} onChange={({ postcssValuesVariables }) => setPostcssExportText(postcssValuesVariables)} />
-        <h3>CSS stylesheet</h3>
-        <textarea className={styles.cssOutputText} value={cssExportText} onChange={({ cssClasses }) => setCssExportText(cssClasses)} />
+        <h3 className={`${styles.postcssExportToggler} ${postcssExportToggled && styles.expandable}`} onClick={handlePostcssExportClick}>PostCSS Values Variables color list <a href="https://github.com/css-modules/css-modules/blob/master/docs/values-variables.md" target="_blank">?</a></h3>
+        <textarea className={`${styles.postcssOutputText} ${postcssExportToggled && styles.hidden}`} value={postcssExportText} onChange={({ postcssValuesVariables }) => setPostcssExportText(postcssValuesVariables)} />
+        <h3 className={`${styles.cssExportToggler} ${cssExportToggled && styles.expandable}`} onClick={handleCssExportClick}>CSS stylesheet</h3>
+        <textarea className={`${styles.cssOutputText} ${cssExportToggled && styles.hidden}`} value={cssExportText} onChange={({ cssClasses }) => setCssExportText(cssClasses)} />
       </section>
     </>
   );
