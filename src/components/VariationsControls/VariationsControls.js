de6183c22a7393a2b4def6c450546b71bc421c72
Ben Beekman <bbeekman@gmail.com>
2020-06-15 22:40:01 -0700

    darkmode style tweaks and fix for postcss hsl exported color values

--- src/components/VariationsControls/VariationsControls.js
+++ src/components/VariationsControls/VariationsControls.js
@@ -115,7 +115,7 @@ const VariationsControls = (color) => {
         const key = (color.matchType);
         let colorString = hslToHex(color.h, color.s, color.l);
         if(exportHslToggled) {
-          colorString = ('hsl(' + color.h.toFixed(0) + ', ' + (color.s * 100).toFixed(2) + '%, ' + ((color.l * 100).toFixed(2)) + '%;');
+          colorString = ('hsl(' + color.h.toFixed(0) + ', ' + (color.s * 100).toFixed(2) + '%, ' + ((color.l * 100).toFixed(2)) + '%) ;');
         }
         if(exportRgbToggled) {
           const rgb = hslToRgb(color.h, color.s, color.l);
