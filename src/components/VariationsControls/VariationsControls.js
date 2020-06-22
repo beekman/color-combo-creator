50cb57396af5d8d9eb7d6d0d01349544dd7bea13
Ben Beekman <bbeekman@gmail.com>
2020-06-21 17:25:26 -0700

    refactored palette export functions into external script

--- src/components/VariationsControls/VariationsControls.js
+++ src/components/VariationsControls/VariationsControls.js
@@ -8,6 +8,9 @@ import {
 import {
   hslToRgb, hslToHex, hslToObject, hslToString, rgbToString
 } from '../../utils/colorConverters';
+import {
+  getPostcssValuesVariables, getCssClasses
+} from '../../utils/paletteExporters';
 import { MdInvertColors, MdBrightnessLow, MdFormatColorReset, MdLinearScale } from 'react-icons/md';
 import { IoIosColorFilter } from 'react-icons/io';
 import { TiAdjustBrightness } from 'react-icons/ti';
@@ -113,90 +116,42 @@ const VariationsControls = (color) => {
   let darkerSwatches = makeColorSwatches(hslDarkers);
   let desaturatedSwatches = makeColorSwatches(hslDesaturateds);
 
-  const getPostcssValuesVariables = (colorSet, exportHexToggled, exportHslToggled, exportRgbToggled) => {
-    let postCSSVariables = '';
-    if(colorSet.length > 0) {
-      colorSet.map((color) => {
+  return (
+    <>
+      <div className={styles.VariationsControls} style={{ background: `hsl(${(color.color.h)}, ${color.color.s * 100}%, ${color.color.l * 150}%)` }}>
+        <label htmlFor="harmonyQuantity" title="Complementary colors, evenly spaced around the color wheel, relative to the base hue. 2 will give you a split complementary triad scheme, 3 will return a color from each quarter of the color wheel."><IoIosColorFilter /><span className={styles.textLabel}>Harmonies</span></label><input type="number" id="harmonyQuantity" value={harmonyQuantity} min="0" max="36" onChange={({ target }) => setHarmonyQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFFFFF' : '#111')}` }} />
+        <label htmlFor="inverseQuantity" title="Colors opposite from the base & harmonic colors on the color wheel. First color is inverted base, subsequent colors are inverted harmonies."><MdInvertColors /><span className={styles.textLabel}>Inverses</span></label><input type="number" id="inverseQuantity" min="0" max={inverseMax} value={inverseQuantity} onChange={({ target }) => setInverseQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFF' : '#111')}` }} />
+        <label htmlFor="analogousQuantity" title="Colors similar in hue to the base color. Each color is 30 degrees away from the base color or nearest analogous color."><MdLinearScale /><span className={styles.textLabel}>Analogous</span></label><input type="number" id="analogousQuantity" min="0" max="12" value={analogousQuantity} onChange={({ target }) => setAnalogousQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFF' : '#111')}` }} />
+        <label htmlFor="lighterQuantity" title="Lighter color sets to generate from the base, harmonies, and inverses, with each increment stepping closer to white."><MdBrightnessLow /><span className={styles.textLabel}>Lighter</span> &times;<input type="number" id="lighterQuantity" value={lighterQuantity} min="0" max="20" onChange={({ target }) => setLighterQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFF' : '#111')}` }} /></label>
+        <label htmlFor="darkerQuantity" title="Darker color sets to generate from the base, harmonies, and inverses,with each increment stepping closer to black."> <TiAdjustBrightness /><span className={styles.textLabel}>Darker</span> &times;<input type="number" id="darkerQuantity" min="0" max="20" value={darkerQuantity} onChange={({ target }) => setDarkerQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFF' : '#111')}` }} /></label>
+        <label htmlFor="desaturatedQuantity" title="Less saturated color sets to generate from the base, harmonies, and inverses, with each increment stepping closer to grayscale."><MdFormatColorReset /><span className={styles.textLabel}>Desaturated</span> &times;<input type="number" id="desaturatedQuantity" value={desaturatedQuantity} min="0" max="20" onChange={({ target }) => setDesaturatedQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFF' : '#111')}` }} /></label>
+      </div>
+      <section className={styles.colorFormats} style={{ borderColor: `hsl(${(getOppositeDegree(color.color.h))}, ${color.color.s * 100}%, ${color.color.l * 25}%)`, borderWidth: '0 2px 2px', background: `hsl(${(getOppositeDegree(color.color.h))}, ${color.color.s * 100}%, ${color.color.l * 50}%)`, borderStyle: 'solid' }}>
+        <div className={`${styles.exportFormatToggle} ${(!swatchToggled) && styles.toggled}`} onClick={handleShowColorsClick}>View Colors As:</div>
+        <div className={`${styles.exportFormatToggle} ${exportHexToggled && styles.toggled}`} onClick={handleExportHexClick}>Hex</div>
+        <div className={`${styles.exportFormatToggle} ${exportRgbToggled && styles.toggled}`} onClick={handleExportRgbClick}>RGB</div>
+        <div className={`${styles.exportFormatToggle} ${exportHslToggled && styles.toggled}`} onClick={handleExportHslClick}>HSL</div>
+        <DarkModeToggle />
+      </section>
 
-        const key = (color.matchRelationship + (Number(i) + 1));
-        const hslString = hslToString(color);
-        const rgb = hslToRgb(color.h, color.s, color.l);
-        const rgbString = rgbToString(rgb);
-        const hexString = hslToHex(color.h, color.s, color.l);
-        let colorString = hexString;
-        if(exportHslToggled) colorString = hslString;
-        if(exportRgbToggled) colorString = rgbString;
-        if(exportHexToggled) colorString = hexString;
-        
-        const line = `@value ${key}: ${colorString}\n`;
-        postCSSVariables += line;
-      });
-    }
-    return postCSSVariables;
-  };
+      <section className={styles.ColorMatches}>
+        {harmonySwatches}
+        {inverseSwatches}
+        {analogousSwatches}
+        {lighterSwatches}
+        {darkerSwatches}
+        {desaturatedSwatches}
+      </section>
 
-  const getCssClasses = (colorSet, exportHexToggled, exportHslToggled, exportRgbToggled) => {
-    let cssStyles = '';
-    const key = (color.matchRelationship + (Number(i) + 1));
-    const hslString = hslToString(color);
-    const rgb = hslToRgb(color.h, color.s, color.l);
-    const rgbString = rgbToString(rgb);
-    const hexString = hslToHex(color.h, color.s, color.l);
-    let colorString = hexString;
-    if(colorSet.length > 0) {
-      colorSet.map((color) => {
-        const key = color.matchRelationship;
-        let colorString = hexString;
-        if(exportHslToggled) colorString = hslString;
-        if(exportRgbToggled) colorString = rgbString;
-        if(exportHexToggled) colorString = hexString;
-
-      let line = `.${key}-color {\n\tcolor: ${colorString}; \n}\n`;
-      line = line + `.${key}-bg {\n\tbackground-color: ${colorString}; \n}\n`;
-      line = line + `.${key}-border {\n\tborder-color: ${colorString}; \n}\n\n`;
-      cssStyles += line;
-    });
-  };
-  return cssStyles;
-};
-
-
-return (
-  <>
-    <div className={styles.VariationsControls} style={{ background: `hsl(${(color.color.h)}, ${color.color.s * 100}%, ${color.color.l * 150}%)` }}>
-      <label htmlFor="harmonyQuantity" title="Complementary colors, evenly spaced around the color wheel, relative to the base hue. 2 will give you a split complementary triad scheme, 3 will return a color from each quarter of the color wheel."><IoIosColorFilter /><span className={styles.textLabel}>Harmonies</span></label><input type="number" id="harmonyQuantity" value={harmonyQuantity} min="0" max="36" onChange={({ target }) => setHarmonyQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFFFFF' : '#111')}` }} />
-      <label htmlFor="inverseQuantity" title="Colors opposite from the base & harmonic colors on the color wheel. First color is inverted base, subsequent colors are inverted harmonies."><MdInvertColors /><span className={styles.textLabel}>Inverses</span></label><input type="number" id="inverseQuantity" min="0" max={inverseMax} value={inverseQuantity} onChange={({ target }) => setInverseQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFF' : '#111')}` }} />
-      <label htmlFor="analogousQuantity" title="Colors similar in hue to the base color. Each color is 30 degrees away from the base color or nearest analogous color."><MdLinearScale /><span className={styles.textLabel}>Analogous</span></label><input type="number" id="analogousQuantity" min="0" max="12" value={analogousQuantity} onChange={({ target }) => setAnalogousQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFF' : '#111')}` }} />
-      <label htmlFor="lighterQuantity" title="Lighter color sets to generate from the base, harmonies, and inverses, with each increment stepping closer to white."><MdBrightnessLow /><span className={styles.textLabel}>Lighter</span> &times;<input type="number" id="lighterQuantity" value={lighterQuantity} min="0" max="20" onChange={({ target }) => setLighterQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFF' : '#111')}` }} /></label>
-      <label htmlFor="darkerQuantity" title="Darker color sets to generate from the base, harmonies, and inverses,with each increment stepping closer to black."> <TiAdjustBrightness /><span className={styles.textLabel}>Darker</span> &times;<input type="number" id="darkerQuantity" min="0" max="20" value={darkerQuantity} onChange={({ target }) => setDarkerQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFF' : '#111')}` }} /></label>
-      <label htmlFor="desaturatedQuantity" title="Less saturated color sets to generate from the base, harmonies, and inverses, with each increment stepping closer to grayscale."><MdFormatColorReset /><span className={styles.textLabel}>Desaturated</span> &times;<input type="number" id="desaturatedQuantity" value={desaturatedQuantity} min="0" max="20" onChange={({ target }) => setDesaturatedQuantity(target.value)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#FFF' : '#111')}` }} /></label>
-    </div>
-    <section className={styles.colorFormats} style={{ borderColor: `hsl(${(getOppositeDegree(color.color.h))}, ${color.color.s * 100}%, ${color.color.l * 25}%)`, borderWidth: '0 2px 2px', background: `hsl(${(getOppositeDegree(color.color.h))}, ${color.color.s * 100}%, ${color.color.l * 50}%)`, borderStyle: 'solid' }}>
-      <div className={`${styles.exportFormatToggle} ${(!swatchToggled) && styles.toggled}`} onClick={handleShowColorsClick}>View Colors As:</div>
-      <div className={`${styles.exportFormatToggle} ${exportHexToggled && styles.toggled}`} onClick={handleExportHexClick}>Hex</div>
-      <div className={`${styles.exportFormatToggle} ${exportRgbToggled && styles.toggled}`} onClick={handleExportRgbClick}>RGB</div>
-      <div className={`${styles.exportFormatToggle} ${exportHslToggled && styles.toggled}`} onClick={handleExportHslClick}>HSL</div>
-      <DarkModeToggle />
-    </section>
-
-    <section className={styles.ColorMatches}>
-      {harmonySwatches}
-      {inverseSwatches}
-      {analogousSwatches}
-      {lighterSwatches}
-      {darkerSwatches}
-      {desaturatedSwatches}
-    </section>
-
-    <section className={styles.export}>
-      <h2>Palette Export Options</h2>
-      <h3 className={`${styles.postcssExportToggler} ${postcssExportToggled && styles.expandable}`} onClick={handlePostcssExportClick}>PostCSS Values Variables color list<a href="https://github.com/css-modules/css-modules/blob/master/docs/values-variables.md" target="_blank">*</a></h3>
-      <textarea className={`${styles.postcssOutputText} ${postcssExportToggled && styles.hidden}`} value={postcssExportText} onChange={({ postcssValuesVariables }) => setPostcssExportText(postcssValuesVariables)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#EEE' : '#111')}` }} />
-      <h3 className={`${styles.cssExportToggler} ${cssExportToggled && styles.expandable}`} onClick={handleCssExportClick}>CSS stylesheet</h3>
-      <textarea className={`${styles.cssOutputText} ${cssExportToggled && styles.hidden}`} value={cssExportText} onChange={({ cssClasses }) => setCssExportText(cssClasses)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#EEE' : '#111')}` }} />
-    </section>
-  </>
-);
+      <section className={styles.export}>
+        <h2>Palette Export Options</h2>
+        <h3 className={`${styles.postcssExportToggler} ${postcssExportToggled && styles.expandable}`} onClick={handlePostcssExportClick}>PostCSS Values Variables color list<a href="https://github.com/css-modules/css-modules/blob/master/docs/values-variables.md" target="_blank">*</a></h3>
+        <textarea className={`${styles.postcssOutputText} ${postcssExportToggled && styles.hidden}`} value={postcssExportText} onChange={({ postcssValuesVariables }) => setPostcssExportText(postcssValuesVariables)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#EEE' : '#111')}` }} />
+        <h3 className={`${styles.cssExportToggler} ${cssExportToggled && styles.expandable}`} onClick={handleCssExportClick}>CSS stylesheet</h3>
+        <textarea className={`${styles.cssOutputText} ${cssExportToggled && styles.hidden}`} value={cssExportText} onChange={({ cssClasses }) => setCssExportText(cssClasses)} style={{ background: `${(darkMode.value ? '#111' : '#FFF')}`, color: `${(darkMode.value ? '#EEE' : '#111')}` }} />
+      </section>
+    </>
+  );
 };
 
 export default VariationsControls;
---
+++ src/utils/paletteExporters.js
@@ -0,0 +1,46 @@
+import {
+  hslToRgb, hslToHex
+} from './colorConverters';
+export const getPostcssValuesVariables = (colorSet, exportHexToggled, exportHslToggled, exportRgbToggled) => {
+  let postCSSVariables = '';
+  if(colorSet.length > 0) {
+    colorSet.map((color) => {
+      const key = (color.matchRelationship + (Number(i) + 1));
+      const hslString = hslToString(color);
+      const rgb = hslToRgb(color.h, color.s, color.l);
+      const rgbString = rgbToString(rgb);
+      const hexString = hslToHex(color.h, color.s, color.l);
+      let colorString = hexString;
+      if(exportHslToggled) colorString = hslString;
+      if(exportRgbToggled) colorString = rgbString;
+      if(exportHexToggled) colorString = hexString;
+
+      const line = `@value ${key}: ${colorString}\n`;
+      postCSSVariables += line;
+    });
+  }
+  return postCSSVariables;
+};
+
+export const getCssClasses = (colorSet, exportHexToggled, exportHslToggled, exportRgbToggled) => {
+  let cssStyles = '';
+  if(colorSet.length > 0) {
+    colorSet.map((color) => {
+      const key = (color.matchRelationship + (Number(i) + 1));
+      const hslString = hslToString(color);
+      const rgb = hslToRgb(color.h, color.s, color.l);
+      const rgbString = rgbToString(rgb);
+      const hexString = hslToHex(color.h, color.s, color.l);
+      let colorString = hexString;
+      if(exportHslToggled) colorString = hslString;
+      if(exportRgbToggled) colorString = rgbString;
+      if(exportHexToggled) colorString = hexString;
+
+      let line = `.${key} {\n\tcolor: ${colorString}; \n}\n`;
+      line = line + `.${key}-bg {\n\tbackground-color: ${colorString}; \n}\n`;
+      line = line + `.${key}-border {\n\tborder-color: ${colorString}; \n}\n\n`;
+      cssStyles += line;
+    });
+  }
+  return cssStyles;
+};
