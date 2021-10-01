6ea1d12b91bbba3184d4c21572e2a07880211b64
Ben Beekman <bbeekman@gmail.com>
2021-09-30 20:52:54 -0700

    footer edit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,9 @@ import styles from './Footer.css';
 const Footer = () => {
   return (
     <footer className={styles.Footer}>
-      <p>&copy;Ben Beekman, 2021.</p>
+      <p>&copy;Ben Beekman 2021.</p>
     </footer>
   );
 };
-
-Footer.propTypes = {};
 
 export default Footer;
