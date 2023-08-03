f56dffcd19b7850e6c062295479c35708acc8c4f
Ben Beekman <bbeekman@gmail.com>
2023-08-03 15:51:34 -0700

    eslint revisions

--- src/utils/paletteExporters.js
+++ src/utils/paletteExporters.js
@@ -3,7 +3,7 @@ import {
 } from './colorConverters';
 export const getPostcssValuesVariables = (colorSet, exportHexToggled, exportHslToggled, exportRgbToggled) => {
   let postCSSVariables = '';
-  if(colorSet.length > 0) {
+  if (colorSet.length > 0) {
     colorSet.map((color) => {
       const key = (color.matchRelationship);
       const hslString = hslToString(color);
@@ -11,9 +11,9 @@ export const getPostcssValuesVariables = (colorSet, exportHexToggled, exportHslT
       const rgbString = rgbToString(rgb);
       const hexString = hslToHex(color.h, color.s, color.l);
       let colorString = hexString;
-      if(exportHslToggled) colorString = hslString;
-      if(exportRgbToggled) colorString = rgbString;
-      if(exportHexToggled) colorString = hexString;
+      if (exportHslToggled) colorString = hslString;
+      if (exportRgbToggled) colorString = rgbString;
+      if (exportHexToggled) colorString = hexString;
 
       const line = `@value ${key}: ${colorString}\n`;
       postCSSVariables += line;
@@ -24,17 +24,17 @@ export const getPostcssValuesVariables = (colorSet, exportHexToggled, exportHslT
 
 export const getCssClasses = (colorSet, exportHexToggled, exportHslToggled, exportRgbToggled) => {
   let cssStyles = '';
-  if(colorSet.length > 0) {
-    colorSet.map((color, i) => {
+  if (colorSet.length > 0) {
+    colorSet.map((color) => {
       const key = (color.matchRelationship);
       const hslString = hslToString(color);
       const rgb = hslToRgb(color.h, color.s, color.l);
       const rgbString = rgbToString(rgb);
       const hexString = hslToHex(color.h, color.s, color.l);
       let colorString = hexString;
-      if(exportHslToggled) colorString = hslString;
-      if(exportRgbToggled) colorString = rgbString;
-      if(exportHexToggled) colorString = hexString;
+      if (exportHslToggled) colorString = hslString;
+      if (exportRgbToggled) colorString = rgbString;
+      if (exportHexToggled) colorString = hexString;
 
       let line = `.${key} {\n\tcolor: ${colorString}; \n}\n`;
       line = line + `.${key}-bg {\n\tbackground-color: ${colorString}; \n}\n`;
